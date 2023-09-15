8ec8668a8e6aa2f85ec43cc206cc560a17efdfdc
Vinay Bansal <vinaybansal@Vinays-MacBook-Air.local>
2023-09-15 20:44:53 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,31 @@
+const parent = React.createElement(
+  "div",
+  {
+    id: "parent",
+  },
+  [
+    React.createElement("div", { id: "child" }, [
+      React.createElement("h1", {}, "Hello From React!"),
+      React.createElement("h2", {}, "I am h2 elemt"),
+    ]),
+    React.createElement("div", { id: "child2" }, [
+      React.createElement("h1", {}, "Hello From React!"),
+      React.createElement("h2", {}, "I am h2 elemt"),
+    ]),
+  ]
+);
+
+const heading = React.createElement(
+  "h1",
+  { id: "heading", xyz: "abc" },
+  "Hello World From React"
+);
+
+console.log(heading);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+console.log(root);
+console.log(typeof root);
+
+root.render(parent);
